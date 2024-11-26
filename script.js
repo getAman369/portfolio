5473fc43472b56912eec6fcfece679049eb4677b
getAman369 <ayadav99672@gmail.com>
2024-11-26 20:15:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.querySelectforAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click',function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+
+        const targetElement = document.getElementById(targetId);
+
+        window.scrollTo({
+            top:
+            targetElement.offsetTop - 50,
+            behavior: 'smooth'
+        });
+    });
+});
